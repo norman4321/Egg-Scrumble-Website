fd04182448b85af53d35addf346ae2e072698379
norman4321 <72541828+norman4321@users.noreply.github.com>
2020-12-07 12:45:30 +0800

    Update controllers.js

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -1,5 +1,6 @@
 const User = require('../models/User');
 const jwt = require('jsonwebtoken');
+const nodemailer = require('nodemailer');
 
 // handle errors
 const handleErrors = (err) => {
@@ -10,24 +11,24 @@ const handleErrors = (err) => {
     };
 
     //incorrect email
-    if(err.message == 'incorrect email'){
+    if (err.message == 'incorrect email') {
         errors.email = 'incorrect email or password';
     }
 
     //incorrect password
-    if(err.message == 'incorrect password'){
+    if (err.message == 'incorrect password') {
         errors.password = 'incorrect email or password';
     }
 
     // duplicate error code
-    if(err.code === 11000) {
+    if (err.code === 11000) {
         errors.email = "email is already registered";
         return errors;
     }
 
     // validation errors
-    if(err.message.includes('user validation failed')) {
-        Object.values(err.errors).forEach(({properties}) => {
+    if (err.message.includes('user validation failed')) {
+        Object.values(err.errors).forEach(({ properties }) => {
             errors[properties.path] = properties.message;
         });
     }
@@ -37,92 +38,125 @@ const handleErrors = (err) => {
 const maxAge = 3 * 24 * 60 * 60; // 3 days to seconds
 
 const createToken = (id) => {
-    return jwt.sign({id}, 'egg-book-secret', {
+    return jwt.sign({ id }, 'egg-book-secret', {
         expiresIn: maxAge
     })
 }
 
-const signup_get = (req,res) => {
-    res.render('signup', {title: 'Sign up'});
+const signup_get = (req, res) => {
+    res.render('signup', { title: 'Sign up' });
 }
 
-const login_get = (req,res) => {
-    res.render('login', {title: 'Log in'});
+const login_get = (req, res) => {
+    res.render('login', { title: 'Log in' });
 }
 
-const signup_post = async (req,res) => {
-    const {email, password, first_name, last_name} = req.body;
-    
+const signup_post = async(req, res) => {
+    const { email, password, first_name, last_name } = req.body;
+
     try {
-        const user = await User.create({email, password, first_name, last_name});
+        const user = await User.create({ email, password, first_name, last_name });
         const token = createToken(user._id);
-        res.cookie('jwt', token, {httpOnly:true, maxAge: maxAge * 1000});
-        res.status(201).json({user: user._id});
-    }
-    catch (err) {
+        res.cookie('jwt', token, { httpOnly: true, maxAge: maxAge * 1000 });
+        res.status(201).json({ user: user._id });
+    } catch (err) {
         const errors = handleErrors(err);
         res.status(400).send({ errors });
     }
 }
 
-const login_post = async (req,res) => {
-    const {email, password} = req.body;
+const login_post = async(req, res) => {
+    const { email, password } = req.body;
 
-    try{
+    try {
         const user = await User.login(email, password);
         const token = createToken(user._id);
-        res.cookie('jwt', token, {httpOnly:true, maxAge: maxAge * 1000});
-        res.status(200).json({user: user._id});
-    } catch(err) {
+        res.cookie('jwt', token, { httpOnly: true, maxAge: maxAge * 1000 });
+        res.status(200).json({ user: user._id });
+    } catch (err) {
         const errors = handleErrors(err);
-        res.status(400).send({errors});
+        res.status(400).send({ errors });
     }
 }
 
-const logout_get = async (req,res) => {
-    res.cookie('jwt', '', { maxAge: 1});
+const logout_get = async(req, res) => {
+    res.cookie('jwt', '', { maxAge: 1 });
     res.redirect('/');
 }
 
-const index_get = async (req,res) => {
-    res.render('index', {title:'Home'});
-}   
-
-const bookmarks_get = async (req,res) => {
-    res.render('bookmarks', {title:'Bookmarks'});
+const index_get = async(req, res) => {
+    res.render('index', { title: 'Home' });
 }
 
-const owned_get = async (req,res) => {
-    res.render('owned', {title:'Owned'});
+const bookmarks_get = async(req, res) => {
+    res.render('bookmarks', { title: 'Bookmarks' });
 }
 
-const forgotpass_get = async (req,res) => {
-    res.render('forgotpass', {title: 'Forgot Password'});
+const owned_get = async(req, res) => {
+    res.render('owned', { title: 'Owned' });
 }
+
+const forgotpass_get = async(req, res) => {
+    res.render('forgotpass', { title: 'Forgot Password' });
+}
+
 const forgotpass_post = async(req, res) => {
     const { email } = req.body;
 
     User.exists({ email: email }, function(err, doc) {
         if (doc) {
-            res.redirect('/forgotpasscode')
+
+            let code = Math.floor(1000 + Math.random() * 9000);
+            let transporter = nodemailer.createTransport({
+                service: 'gmail',
+                auth: {
+                    user: 'eggbookemailer@gmail.com',
+                    pass: 'Admin123!'
+                }
+            });
+            User.findOneAndUpdate({ email }, { code })
+            let mailOptions = {
+                from: 'eggbookemailer@gmail.com_noreply',
+                to: `${email}`,
+                subject: 'Password Reset Request',
+                text: "Your Verification code is " + `${code}` + " Use this code within 2 hours to reset your password"
+
+            };
+
+            transporter.sendMail(mailOptions, function(error, info) {
+                if (error) {
+                    console.log(error);
+                } else {
+                    console.log('Email sent: ' + info.response);
+                }
+            });
+            res.render('forgotpasscode', { email: email, title: "Enter Code" })
+
         } else {
             res.redirect(400, '/forgotpass')
-                
+
         }
 
     });
 
+
 }
 
-const forgotpasscode_get = async (req,res) => {
-    res.render('forgotpasscode', {title: 'Forgot Password'});
+const forgotpasscode_get = async(req, res) => {
+    res.render('forgotpasscode', { title: 'Forgot Password' });
 }
 
-const enterpassword_get = async (req,res) => {
-    res.render('enterpassword', {title: 'Enter Password'});
+const forgotpasscode_post = async(req, res) => {
+    const { email } = req.body;
+    
+
+}
+
+const enterpassword_get = async(req, res) => {
+    res.render('enterpassword', { title: 'Enter Password' });
 }
 module.exports = {
-    signup_get, 
+    signup_get,
     signup_post,
     login_get,
     login_post,
@@ -133,5 +167,6 @@ module.exports = {
     forgotpass_get,
     forgotpass_post,
     forgotpasscode_get,
+    forgotpasscode_post,
     enterpassword_get
 }
